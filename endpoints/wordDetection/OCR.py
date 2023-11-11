b65e30f7ffda4f598b3f94ad71a67de8331118d3
edgar <edgar99carbajal@gmail.com>
2023-11-11 09:00:00 -0800

    trying new reqs

--- endpoints/wordDetection/OCR.py
+++ endpoints/wordDetection/OCR.py
@@ -1,40 +1,40 @@
-# app.py
-import os
-from flask import Flask, request, jsonify, Blueprint
-from PIL import Image
-import torch
-import torchvision.transforms as transforms
-import pytesseract
+# # app.py
+# import os
+# from flask import Flask, request, jsonify, Blueprint
+# from PIL import Image
+# import torch
+# import torchvision.transforms as transforms
+# import pytesseract
 
 
-def ocr(file):
-    # Check if the request contains a file
-    if 'file' not in request.files:
-        return jsonify({"error": "No file part"}), 400
+# def ocr(file):
+#     # Check if the request contains a file
+#     if 'file' not in request.files:
+#         return jsonify({"error": "No file part"}), 400
 
-    file = request.files['file']
+#     file = request.files['file']
 
-    # Check if the file is a JPEG
-    if file and file.filename.endswith(('.jpeg', '.jpg', '.png')):
-        try:
-            # Save the uploaded image temporarily
-            image_path = "temp.jpg"
-            file.save(image_path)
+#     # Check if the file is a JPEG
+#     if file and file.filename.endswith(('.jpeg', '.jpg', '.png')):
+#         try:
+#             # Save the uploaded image temporarily
+#             image_path = "temp.jpg"
+#             file.save(image_path)
 
-            # Load the image using PIL
-            img = Image.open(image_path)
+#             # Load the image using PIL
+#             img = Image.open(image_path)
 
-            # Perform OCR using pytesseract
-            text = pytesseract.image_to_string(img)
+#             # Perform OCR using pytesseract
+#             text = pytesseract.image_to_string(img)
 
-            # You can also perform additional processing using PyTorch and other libraries here
-            # For more advanced OCR, consider using a dedicated OCR library like Tesseract
+#             # You can also perform additional processing using PyTorch and other libraries here
+#             # For more advanced OCR, consider using a dedicated OCR library like Tesseract
 
-            # Clean up the temporary image
-            os.remove(image_path)
+#             # Clean up the temporary image
+#             os.remove(image_path)
 
-            return jsonify({"text": text})
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
-    else:
-        return jsonify({"error": "Invalid file format. Please upload a JPEG image."}), 400
+#             return jsonify({"text": text})
+#         except Exception as e:
+#             return jsonify({"error": str(e)}), 500
+#     else:
+#         return jsonify({"error": "Invalid file format. Please upload a JPEG image."}), 400
